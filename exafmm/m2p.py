bc4b46e70e87b7c060a44d583e666c0ce6f2a838
Andreas Kloeckner <inform@tiker.net>
2011-05-18 18:34:03 +0200

    Various fixes to M2P.

--- exafmm/m2p.py
+++ exafmm/m2p.py
@@ -7,15 +7,17 @@ from pytools import memoize_method
 import pyopencl as cl
 import pyopencl.array as cl_array
 
-from exafmm.kernel_common import COMMON_PREAMBLE
+from exafmm.kernel_common import (
+        COMMON_PREAMBLE, FMMParameters)
+
 
 
 
 M2P_KERNEL = Template(
 COMMON_PREAMBLE +
-"""
-typedef uint offset_t;
-typedef uint mpole_offset_t;
+"""//CL//
+typedef ${offset_type} offset_t;
+typedef ${mpole_offset_type} mpole_offset_t;
 typedef ${coefficient_type} coeff_t;
 typedef ${geometry_type} geometry_t;
 typedef ${geometry_type}${dimensions} geometry_vec_t;
@@ -25,10 +27,18 @@ typedef ${output_type} output_t;
 
 
 #define TGT_CELL get_group_id(0)
-<% wg_size = coef_cnt_padded*par_cell_cnt %>
+<% wg_size = coeff_cnt_padded*par_cell_cnt %>
+
+// Work partition:
+// ---------------
+// Each work item is responsible for computing the summed-up multipole expansions
+// at one target point. Each work group is responsible for one target cell.
+// To achieve the, the kernel loops first over batches of particles within the target
+// cell (if necessary) and then over source cells. The outer "loop over source
+// cell batches" orchestrates cooperative loading of multipole coefficients.
 
 __kernel
-__attribute__((reqd_work_group_size(${coef_cnt_padded}, ${par_cell_cnt}, 1)))
+__attribute__((reqd_work_group_size(${coeff_cnt_padded}, ${par_cell_cnt}, 1)))
 void m2p(
 % for i in range(dimensions):
   global const geometry_t *t${i}_g,
@@ -44,67 +54,68 @@ void m2p(
   )
 
 {
-  uint lid = get_local_id(0) + ${coef_cnt_padded} * get_local_id(1);
+    uint lid = get_local_id(0) + ${coeff_cnt_padded} * get_local_id(1);
 
-  offset_t ilist_start = m2p_ilist_starts_g[TGT_CELL];
-  offset_t ilist_end = m2p_ilist_starts_g[TGT_CELL+1];
+    offset_t ilist_start = m2p_ilist_starts_g[TGT_CELL];
+    offset_t ilist_end = m2p_ilist_starts_g[TGT_CELL+1];
 
-  offset_t tgt_cell_particle_offset = cell_idx_to_particle_offset_g[TGT_CELL];
-  uint tgt_cell_particle_count = cell_idx_to_particle_cnt_g[TGT_CELL];
+    offset_t tgt_cell_particle_offset = cell_idx_to_particle_offset_g[TGT_CELL];
+    uint tgt_cell_particle_count = cell_idx_to_particle_cnt_g[TGT_CELL];
 
-  __local coeff_t mpole_coeff_l[${par_cell_cnt} * ${coef_cnt_padded}];
+    __local coeff_t mpole_coeff_l[${par_cell_cnt} * ${coeff_cnt_padded}];
 
-  // index into this cell's list of particles
-  uint plist_idx = lid;
+    // index into this cell's list of particles
+    uint plist_idx = lid;
 
-  // loop over particle batches
-  while (plist_idx < tgt_cell_particle_count)
-  {
-    geometry_vec_t tgt;
-    ${load_vector("tgt", "t", "tgt_cell_particle_offset + plist_idx")}
-
-    % for i in range(output_count):
-      output_t output${i} = 0;
-    % endfor
-
-    // index into overall M2P interaction list
-    offset_t ilist_idx = ilist_start + get_local_id(1);
-
-    // loop over source cell batches
-    while (ilist_idx < ilist_end)
+    // loop over particle batches
+    while (plist_idx < tgt_cell_particle_count)
     {
-      barrier(CLK_LOCAL_MEM_FENCE);
-      mpole_offset_t mpole_offset = m2p_ilist_mpole_offsets_g[ilist_idx];
-      mpole_coeff_l[lid] = mpole_coeff_g[mpole_offset + get_local_id(0)];
-      barrier(CLK_LOCAL_MEM_FENCE);
+        geometry_vec_t tgt;
+        ${load_vector("tgt", "t", "tgt_cell_particle_offset + plist_idx")}
 
-      for (uint src_cell = 0; src_cell < ${par_cell_cnt}; ++src_cell)
-      {
-        uint loc_mpole_base = src_cell*${coef_cnt_padded};
-        geometry_vec_t ctr;
-        % for i in range(dimensions):
-          ctr.s${i} = mpole_coeff_l[loc_mpole_base+${i}];
+        % for i in range(output_count):
+            output_t output${i} = 0;
         % endfor
-        loc_mpole_base += ${dimensions};
 
-        % for var, expr in vars_and_exprs:
-          % if var.startswith("output"):
-            ${var} = ${expr};
-          % else:
-            output_t ${var} = ${expr};
-          % endif
+        // index into overall M2P interaction list
+        offset_t ilist_idx = ilist_start + get_local_id(1);
+
+        // loop over source cell batches
+        while (ilist_idx < ilist_end)
+        {
+            barrier(CLK_LOCAL_MEM_FENCE);
+            mpole_offset_t mpole_offset = m2p_ilist_mpole_offsets_g[ilist_idx];
+            mpole_coeff_l[lid] = mpole_coeff_g[mpole_offset + get_local_id(0)];
+            barrier(CLK_LOCAL_MEM_FENCE);
+
+            // loop over source cells
+            for (uint src_cell = 0; src_cell < ${par_cell_cnt}; ++src_cell)
+            {
+                uint loc_mpole_base = src_cell*${coeff_cnt_padded};
+                geometry_vec_t ctr;
+                % for i in range(dimensions):
+                    ctr.s${i} = mpole_coeff_l[loc_mpole_base+${i}];
+                % endfor
+                loc_mpole_base += ${dimensions};
+
+                % for var, expr in vars_and_exprs:
+                    % if var.startswith("output"):
+                        ${var} += ${expr};
+                    % else:
+                        output_t ${var} = ${expr};
+                    % endif
+                % endfor
+          }
+
+          ilist_idx += ${par_cell_cnt};
+        }
+
+        % for i in range(output_count):
+          output${i}_g[tgt_cell_particle_offset + plist_idx] = output${i};
         % endfor
-      }
 
-      ilist_idx += ${par_cell_cnt};
+        plist_idx += ${wg_size};
     }
-
-    % for i in range(output_count):
-      output${i}_g[tgt_cell_particle_offset + plist_idx] = output${i};
-    % endfor
-
-    plist_idx += ${wg_size};
-  }
 }
 """, strict_undefined=True, disable_unicode=True)
 
@@ -113,7 +124,7 @@ void m2p(
 
 class M2PKernel(object):
     def __init__(self, ctx, expansion, output_maps=[lambda x: x],
-            options=[], name="m2p"):
+            options=[], name="m2p", fmm_par=FMMParameters()):
         """
         :output_maps: A list of functions which will be applied to basis functions
           in the expansion, each generating a different output array.
@@ -121,14 +132,16 @@ class M2PKernel(object):
         self.context = ctx
         self.expansion = expansion
         self.output_maps = output_maps
+        self.fmm_parameters = fmm_par
 
         self.options = options
         self.name = name
 
-    @memoize_method
-    def get_kernel(self, geometry_dtype, coeff_dtype, output_dtype):
-        coeff_dtype = np.dtype(coeff_dtype)
+        self.max_wg_size = min(dev.max_work_group_size for dev in ctx.devices)
 
+    @memoize_method
+    def get_kernel(self, geometry_dtype, coeff_dtype, output_dtype, 
+            par_cell_cnt, coeff_cnt_padded):
         dimensions = self.expansion.dimensions
 
         from exafmm.symbolic.codegen import generate_cl_statements_from_assignments
@@ -161,18 +174,18 @@ class M2PKernel(object):
                 dimensions=dimensions,
                 vars_and_exprs=vars_and_exprs,
                 type_declarations="",
-                par_cell_cnt=10,
-                coef_cnt_padded=self.expansion.padded_coefficient_count(
-                    coeff_dtype),
+                par_cell_cnt=par_cell_cnt,
+                coeff_cnt_padded=coeff_cnt_padded,
                 output_count=len(self.output_maps),
                 double_support=all(
                     has_double_support(dev) for dev in self.context.devices),
 
+                offset_type=dtype_to_ctype(self.fmm_parameters.offset_type),
+                mpole_offset_type=dtype_to_ctype(self.fmm_parameters.mpole_offset_type),
                 coefficient_type=dtype_to_ctype(coeff_dtype),
                 geometry_type=dtype_to_ctype(geometry_dtype),
                 output_type=dtype_to_ctype(output_dtype),
                 )
-        print kernel_src
 
         prg = cl.Program(self.context, kernel_src).build(self.options)
         kernel = getattr(prg, self.name)
@@ -184,24 +197,60 @@ class M2PKernel(object):
     def __call__(self, targets, m2p_ilist_starts, m2p_ilist_mpole_offsets,
             mpole_coeff,
             cell_idx_to_particle_offset, cell_idx_to_particle_cnt,
-            queue=None, allocator=None, output_dtype=None):
+            output_dtype=None, allocator=None, 
+            queue=None, wait_for=None):
         target_count, = targets[0].shape
+
+        # {{{ type processing
+
+        geometry_dtype = targets[0].dtype
+        coeff_dtype = np.dtype(mpole_coeff.dtype)
+        if output_dtype is None:
+            output_dtype = coeff_dtype
+
+        coeff_cnt_padded = self.expansion.padded_coefficient_count(
+                    coeff_dtype)
+
+        # }}}
+
+        # {{{ determine work group size
+
+        wg_size = min(self.max_wg_size, 256) # FIXME: Tune?
+        from pytools import div_ceil
+        par_cell_cnt = div_ceil(wg_size, coeff_cnt_padded)
+        wg_size = par_cell_cnt * coeff_cnt_padded
+
+        # }}}
 
         queue = queue or targets[0].queue
         allocator = allocator or targets[0].allocator
 
-        geometry_dtype = targets[0].dtype
-        coeff_dtype = mpole_coeff.dtype
-        if output_dtype is None:
-            output_dtype = coeff_dtype
-
         from pytools.obj_array import make_obj_array
         outputs = make_obj_array([
-            cl_array.empty(queue, target_count, output_dtype)
+            cl_array.empty(queue, target_count, output_dtype,
+                allocator=allocator)
             for expr in self.output_maps])
 
-        kernel = self.get_kernel(geometry_dtype, coeff_dtype, output_dtype)
+        tgt_cell_count = len(m2p_ilist_starts) - 1
+        kernel = self.get_kernel(geometry_dtype, coeff_dtype, output_dtype,
+                par_cell_cnt, coeff_cnt_padded)
+
+        wg_dim = np.array([coeff_cnt_padded, par_cell_cnt])
+        global_dim = wg_dim * np.array([tgt_cell_count, 1])
+
+        kernel(queue, global_dim, wg_dim,
+                *(
+                    [tgt_i.data for tgt_i in targets]
+                    + [out_i.data for out_i in outputs]
+                    + [m2p_ilist_starts.data, m2p_ilist_mpole_offsets.data,
+                        mpole_coeff.data, 
+                        cell_idx_to_particle_offset.data,
+                        cell_idx_to_particle_cnt.data]
+                    ), wait_for=wait_for)
 
         return outputs
 
 
+
+
+# vim: foldmethod=marker filetype=pyopencl.python
