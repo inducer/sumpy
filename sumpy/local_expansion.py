a481e07c2fdbac304b754003279df35f966801f7
Andreas Kloeckner <inform@tiker.net>
2012-05-14 15:54:16 -0400

    Add a volume Taylor local expansion.

--- sumpy/local_expansion.py
+++ sumpy/local_expansion.py
@@ -1,10 +1,13 @@
 from __future__ import division
 
 import sympy as sp
+from pytools import memoize_method
 
 
 
 
+
+# {{{ base class
 
 class LocalExpansionBase(object):
     def __init__(self, kernel, order):
@@ -45,14 +48,11 @@ class LocalExpansionBase(object):
     def evaluate(self, coeffs, bvec):
         raise NotImplementedError
 
+# }}}
 
-
+# {{{ line taylor
 
 class LineTaylorLocalExpansion(LocalExpansionBase):
-    @property
-    def coefficient_count(self):
-        return self.order+1
-
     def get_storage_index(self, k):
         return k
 
@@ -83,10 +83,46 @@ class LineTaylorLocalExpansion(LocalExpansionBase):
                 coeffs[self.get_storage_index(i)] / factorial(i)
                 for i in self.get_coefficient_indices())
 
+# }}}
 
+# {{{ volume taylor
 
+class VolumeTaylorLocalExpansion(LocalExpansionBase):
+    @memoize_method
+    def _storage_loc_dict(self):
+        return dict((idx, i) for i, idx in enumerate(self.get_coefficient_indices()))
 
+    def get_storage_index(self, k):
+        return self._storage_loc_dict()[k]
 
+    @memoize_method
+    def get_coefficient_indices(self):
+        from pytools import (
+                generate_nonnegative_integer_tuples_summing_to_at_most
+                as gnitstam)
+
+        return sorted(gnitstam(self.order, self.kernel.dimensions), key=sum)
+
+        return range(self.order+1)
+
+    def coefficients_from_source(self, avec, bvec):
+        from sumpy.tools import mi_derivative
+        ppkernel = self.kernel.postprocess_at_source(
+                self.kernel.get_expression(avec), avec)
+        return [mi_derivative(ppkernel, avec, mi)
+                for mi in self.get_coefficient_indices()]
+
+    def evaluate(self, coeffs, bvec):
+        from sumpy.tools import mi_power, mi_factorial
+        return sum(
+                coeff
+                * self.kernel.postprocess_at_target(mi_power(bvec, mi), bvec)
+                / mi_factorial(mi)
+                for coeff, mi in zip(coeffs, self.get_coefficient_indices()))
+
+# }}}
+
+# {{{ 2D J-expansion
 
 class H2DLocalExpansion(LocalExpansionBase):
     def __init__(self, kernel, order):
@@ -95,10 +131,6 @@ class H2DLocalExpansion(LocalExpansionBase):
                 and kernel.dimensions == 2)
 
         LocalExpansionBase.__init__(self, kernel, order)
-
-    @property
-    def coefficient_count(self):
-        return 2*self.order+1
 
     def get_storage_index(self, k):
         return self.order+k
@@ -130,9 +162,12 @@ class H2DLocalExpansion(LocalExpansionBase):
 
         e_i_ctangle = sp.exp(-sp.I*center_target_angle)
         return sum(
-                self.kernel.postprocess_at_target(
                     coeffs[self.get_storage_index(i)]
-                    * bessel_j(i, sp.Symbol("k")*v)
-                    * e_i_ctangle**i,
-                    bvec)
+                    * self.kernel.postprocess_at_target(
+                        bessel_j(i, sp.Symbol("k")*v)
+                        * e_i_ctangle**i, bvec)
                 for i in self.get_coefficient_indices())
+
+# }}}
+
+# vim: fdm=marker
