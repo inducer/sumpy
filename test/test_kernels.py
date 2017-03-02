2b5c1ce6f82819469a1ce6fc9b15b189823dc058
Andreas Kloeckner <inform@tiker.net>
2017-03-01 20:55:30 -0500

    Import/modernize cleanups in test_kernels

--- test/test_kernels.py
+++ test/test_kernels.py
@@ -1,7 +1,4 @@
-from __future__ import division
-from __future__ import absolute_import
-from __future__ import print_function
-from six.moves import range
+from __future__ import division, absolute_import, print_function
 
 __copyright__ = "Copyright (C) 2012 Andreas Kloeckner"
 
@@ -24,6 +21,8 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 """
+
+from six.moves import range
 
 import numpy as np
 import numpy.linalg as la
