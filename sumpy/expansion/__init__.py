b25c9daa225633f8d4dd1cd4b0c765f6cc422ec3
Andreas Kloeckner <inform@tiker.net>
2017-08-28 10:54:40 -0500

    Add use_rscale to ExpansionBase.with_kernel

--- sumpy/expansion/__init__.py
+++ sumpy/expansion/__init__.py
@@ -95,8 +95,7 @@ class ExpansionBase(object):
     # }}}
 
     def with_kernel(self, kernel):
-        return type(self)(kernel, self.order)
-        # FIXME: add self.use_rscale once the rscale MR is in
+        return type(self)(kernel, self.order, self.use_rscale)
 
     def __len__(self):
         return len(self.get_coefficient_identifiers())
