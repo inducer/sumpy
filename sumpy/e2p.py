83bcc8e24a0afaff1f18dd4a2e917fcf8ac64ccd
xywei <wxy0516@gmail.com>
2018-08-05 16:03:29 +0800

    Handle special cases for rscale

--- sumpy/e2p.py
+++ sumpy/e2p.py
@@ -320,7 +320,12 @@ class E2PFromCSR(E2PBase):
         # meaningfully inferred. Make the type of rscale explicit.
         rscale = centers.dtype.type(kwargs.pop("rscale"))
 
-        return knl(queue, centers=centers, rscale=rscale, **kwargs)
+        try:
+            return knl(queue, centers=centers, rscale=rscale, **kwargs)
+        except TypeError as e:
+            logger.debug("Initial trial of p2e failed, probably the loopy kernel does not accept rscale")
+            logger.debug("Trying to make self-adjustments")
+            return knl(queue, centers=centers, **kwargs)
 
 # }}}
 
