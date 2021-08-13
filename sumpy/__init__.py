1deb69a78375508e2164d02f0bbbaaaec1ff38da
Isuru Fernando <isuruf@gmail.com>
2021-08-13 16:42:01 +0530

    Fix import of M2LUsingTranslationClassesDependentData

--- sumpy/__init__.py
+++ sumpy/__init__.py
@@ -25,6 +25,7 @@ from sumpy.p2p import P2P, P2PFromCSR
 from sumpy.p2e import P2EFromSingleBox, P2EFromCSR
 from sumpy.e2p import E2PFromSingleBox, E2PFromCSR
 from sumpy.e2e import (E2EFromCSR, E2EFromChildren, E2EFromParent,
+    M2LUsingTranslationClassesDependentData,
     M2LGenerateTranslationClassesDependentData, M2LPreprocessMultipole)
 from sumpy.version import VERSION_TEXT
 from pytools.persistent_dict import WriteOncePersistentDict
