cd7b54f8b553cf635c29fa86e8aa31c62d5831fb
Alexandru Fikl <alexfikl@gmail.com>
2022-08-20 21:22:05 +0300

    port examples to arraycontext

--- examples/expansion-toys.py
+++ examples/expansion-toys.py
@@ -1,21 +1,32 @@
-import pyopencl as cl
-import sumpy.toys as t
 import numpy as np
+
+import pyopencl as cl
+
+import sumpy.toys as t
 from sumpy.visualization import FieldPlotter
+from sumpy.kernel import (      # noqa: F401
+        YukawaKernel,
+        HelmholtzKernel,
+        LaplaceKernel)
+
 try:
     import matplotlib.pyplot as plt
-except ModuleNotFoundError:
-    plt = None
+    USE_MATPLOTLIB = True
+except ImportError:
+    USE_MATPLOTLIB = False
 
 
 def main():
-    from sumpy.kernel import (  # noqa: F401
-            YukawaKernel, HelmholtzKernel, LaplaceKernel)
+    from sumpy.array_context import PyOpenCLArrayContext
+    ctx = cl.create_some_context()
+    queue = cl.CommandQueue(ctx)
+    actx = PyOpenCLArrayContext(queue, force_device_scalars=True)
+
     tctx = t.ToyContext(
-            cl.create_some_context(),
-            #LaplaceKernel(2),
+            actx.context,
+            # LaplaceKernel(2),
             YukawaKernel(2), extra_kernel_kwargs={"lam": 5},
-            #HelmholtzKernel(2), extra_kernel_kwargs={"k": 0.3},
+            # HelmholtzKernel(2), extra_kernel_kwargs={"k": 0.3},
             )
 
     pt_src = t.PointSources(
@@ -25,7 +36,7 @@ def main():
 
     fp = FieldPlotter([3, 0], extent=8)
 
-    if 0 and plt is not None:
+    if USE_MATPLOTLIB:
         t.logplot(fp, pt_src, cmap="jet")
         plt.colorbar()
         plt.show()
@@ -35,12 +46,12 @@ def main():
     lexp = t.local_expand(mexp, [3, 0])
     lexp2 = t.local_expand(lexp, [3, 1], 3)
 
-    #diff = mexp - pt_src
-    #diff = mexp2 - pt_src
+    # diff = mexp - pt_src
+    # diff = mexp2 - pt_src
     diff = lexp2 - pt_src
 
     print(t.l_inf(diff, 1.2, center=lexp2.center))
-    if 1 and plt is not None:
+    if USE_MATPLOTLIB:
         t.logplot(fp, diff, cmap="jet", vmin=-3, vmax=0)
         plt.colorbar()
         plt.show()
