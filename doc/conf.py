5cc19191174c364c7b91fd1bd4d0a1780c3c0562
Alexandru Fikl <alexfikl@gmail.com>
2022-09-05 20:22:53 +0300

    add pytools to intersphinx

--- doc/conf.py
+++ doc/conf.py
@@ -18,6 +18,7 @@ release = ver_dic["VERSION_TEXT"]
 intersphinx_mapping = {
     "https://docs.python.org/3/": None,
     "https://numpy.org/doc/stable/": None,
+    "https://documen.tician.de/pytools/": None,
     "https://documen.tician.de/modepy/": None,
     "https://documen.tician.de/pyopencl/": None,
     "https://documen.tician.de/pymbolic/": None,
