3bbd2f3a126482adddd8e842da218e0e121212df
Hirish Chandrasekaran <hirish@ucsb.edu>
2024-11-03 17:58:28 -0600

    Check Helmholtz2D

--- test/test_recurrence.py
+++ test/test_recurrence.py
@@ -1,7 +1,7 @@
 from sumpy.recurrence import get_processed_and_shifted_recurrence, _make_sympy_vec
 import sympy as sp
 import numpy as np
-
+from sympy import hankel1
 from sumpy.expansion.diff_op import (
     laplacian,
     make_identity_diff_op,
@@ -59,11 +59,33 @@ def test_helmholtz_3D():
     
 
 
+def test_helmholtz_2D():
+    w = make_identity_diff_op(2)
+    laplace2d = laplacian(w) + w
+    _,_, r = get_processed_and_shifted_recurrence(laplace2d)
 
+    n = sp.symbols("n")
+    s = sp.Function("s")
 
+    var = _make_sympy_vec("x", 2)
+    var_t = _make_sympy_vec("t", 2)
+    k = 1
+    abs_dist = sp.sqrt((var[0]-var_t[0])**2 + (var[1]-var_t[1])**2)
+    g_x_y = (1j/4) * hankel1(0, k * abs_dist)
+    x_coord = np.random.rand()
+    y_coord = np.random.rand()
+    derivs = [sp.diff(g_x_y, var_t[0], i).subs(var_t[0], 0).subs(var_t[1], 0) for i in range(6)]
+    derivs = [derivs[i].subs(var[0], x_coord).subs(var[1], y_coord).evalf() for i in range(6)]
 
+    check_2_s = r.subs(n, 2).subs(s(0), derivs[0]).subs(s(1), derivs[1]) - derivs[2]
+    check_3_s = r.subs(n, 3).subs(s(0), derivs[0]).subs(s(1), derivs[1]).subs(s(2), derivs[2]) - derivs[3]
+    check_4_s = r.subs(n, 4).subs(s(0), derivs[0]).subs(s(1), derivs[1]).subs(s(2), derivs[2]).subs(s(3), derivs[3]) - derivs[4]
+    check_5_s = r.subs(n, 5).subs(s(0), derivs[0]).subs(s(1), derivs[1]).subs(s(2), derivs[2]).subs(s(3), derivs[3]).subs(s(4), derivs[4]) - derivs[5]
 
-
+    assert abs(check_2_s.subs(var[0],x_coord).subs(var[1],y_coord)) <= 1e-12
+    assert abs(check_3_s.subs(var[0],x_coord).subs(var[1],y_coord)) <= 1e-12
+    assert abs(check_4_s.subs(var[0],x_coord).subs(var[1],y_coord)) <= 1e-12
+    assert abs(check_5_s.subs(var[0],x_coord).subs(var[1],y_coord)) <= 1e-12
 
 def test_laplace_2D():
     w = make_identity_diff_op(2)
