c7f7be75b5a0c306d1ebd44666b8fb63ac1097cb
Hirish Chandrasekaran <hirish@ucsb.edu>
2024-11-03 13:16:12 -0600

    Laplace3D test

--- test/test_recurrence.py
+++ test/test_recurrence.py
@@ -7,12 +7,35 @@ from sumpy.expansion.diff_op import (
     make_identity_diff_op,
 )
 
+def test_laplace_3D():
+    w = make_identity_diff_op(3)
+    laplace3d = laplacian(w)
+    _, _, r = get_processed_and_shifted_recurrence(laplace3d)
+    n = sp.symbols("n")
+    s = sp.Function("s")
+
+    var = _make_sympy_vec("x", 3)
+    var_t = _make_sympy_vec("t", 3)
+    abs_dist = sp.sqrt((var[0]-var_t[0])**2 + (var[1]-var_t[1])**2 + (var[2]-var_t[2])**2)
+    g_x_y = 1/abs_dist
+    derivs = [sp.diff(g_x_y, var_t[0], i).subs(var_t[0], 0).subs(var_t[1], 0).subs(var_t[2], 0) for i in range(6)]
+
+    check_2_s = r.subs(n, 2).subs(s(0), derivs[0]).subs(s(1), derivs[1]) - derivs[2]
+    check_3_s = r.subs(n, 3).subs(s(0), derivs[0]).subs(s(1), derivs[1]).subs(s(2), derivs[2]) - derivs[3]
+    check_4_s = r.subs(n, 4).subs(s(0), derivs[0]).subs(s(1), derivs[1]).subs(s(2), derivs[2]).subs(s(3), derivs[3]) - derivs[4]
+    check_5_s = r.subs(n, 5).subs(s(0), derivs[0]).subs(s(1), derivs[1]).subs(s(2), derivs[2]).subs(s(3), derivs[3]).subs(s(4), derivs[4]) - derivs[5]
+
+    assert abs(abs(check_2_s.subs(var[0], np.random.rand()).subs(var[1], np.random.rand()).subs(var[2], np.random.rand()))) <= 1e-15
+    assert abs(abs(check_3_s.subs(var[0], np.random.rand()).subs(var[1], np.random.rand()).subs(var[2], np.random.rand()))) <= 1e-14
+    assert abs(abs(check_4_s.subs(var[0], np.random.rand()).subs(var[1], np.random.rand()).subs(var[2], np.random.rand()))) <= 1e-12
+    #print(abs(abs(check_5_s.subs(var[0], np.random.rand()).subs(var[1], np.random.rand()).subs(var[2], np.random.rand()))))
+    assert abs(abs(check_5_s.subs(var[0], np.random.rand()).subs(var[1], np.random.rand()).subs(var[2], np.random.rand()))) <= 1e-12
 
 
 def test_helmholtz_3D():
     w = make_identity_diff_op(3)
     helmholtz3d = laplacian(w) + w
-    _,_, r = get_processed_and_shifted_recurrence(helmholtz3d)
+    _, _, r = get_processed_and_shifted_recurrence(helmholtz3d)
 
     n = sp.symbols("n")
     s = sp.Function("s")
@@ -24,11 +47,18 @@ def test_helmholtz_3D():
     derivs = [sp.diff(g_x_y, var_t[0], i).subs(var_t[0], 0).subs(var_t[1], 0).subs(var_t[2], 0) for i in range(6)]
 
 
-    check_2_s = r.subs(n, 2).subs(s(1), derivs[1]).subs(s(0), derivs[0]) - derivs[2]
-    assert abs(check_2_s.subs(var[0], np.random.rand()).subs(var[1], np.random.rand()).subs(var[2], np.random.rand())) <= 1e-12
+    check_2_s = r.subs(n, 2).subs(s(0), derivs[0]).subs(s(1), derivs[1]) - derivs[2]
+    check_3_s = r.subs(n, 3).subs(s(0), derivs[0]).subs(s(1), derivs[1]).subs(s(2), derivs[2]) - derivs[3]
+    check_4_s = r.subs(n, 4).subs(s(0), derivs[0]).subs(s(1), derivs[1]).subs(s(2), derivs[2]).subs(s(3), derivs[3]) - derivs[4]
+    check_5_s = r.subs(n, 5).subs(s(0), derivs[0]).subs(s(1), derivs[1]).subs(s(2), derivs[2]).subs(s(3), derivs[3]).subs(s(4), derivs[4]) - derivs[5]
+
+    assert abs(check_2_s.subs(var[0], np.random.rand()).subs(var[1], np.random.rand()).subs(var[2], np.random.rand())) <= 1e-15
+    assert abs(abs(check_3_s.subs(var[0], np.random.rand()).subs(var[1], np.random.rand()).subs(var[2], np.random.rand()))) <= 1e-14
+    assert abs(abs(check_4_s.subs(var[0], np.random.rand()).subs(var[1], np.random.rand()).subs(var[2], np.random.rand()))) <= 1e-12
+    assert abs(abs(check_5_s.subs(var[0], np.random.rand()).subs(var[1], np.random.rand()).subs(var[2], np.random.rand()))) <= 1e-12
     
 
-test_helmholtz_3D()
+
 
 
 
