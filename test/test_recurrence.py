f79b6d9220008748af552d902d8ea232576ac66f
Hirish Chandrasekaran <hirish@ucsb.edu>
2024-11-04 12:10:26 -0600

    Formatting

--- test/test_recurrence.py
+++ test/test_recurrence.py
@@ -1,13 +1,28 @@
-from sumpy.recurrence import get_processed_and_shifted_recurrence, _make_sympy_vec
-import sympy as sp
+r"""
+With the functionality in this module, we aim to test recurrence
+code.
+
+.. autofunction:: test_laplace3d
+.. autofunction:: test_helmholtz3d
+.. autofunction:: test_laplace2d
+"""
+from __future__ import annotations
+
 import numpy as np
-from sympy import hankel1
+import sympy as sp
+
+# from sympy import hankel1
 from sumpy.expansion.diff_op import (
     laplacian,
     make_identity_diff_op,
 )
+from sumpy.recurrence import _make_sympy_vec, get_processed_and_shifted_recurrence
 
-def test_laplace_3D():
+
+def test_laplace3d():
+    r"""
+    Tests recurrence code for orders up to 6 laplace3d.
+    """
     w = make_identity_diff_op(3)
     laplace3d = laplacian(w)
     _, _, r = get_processed_and_shifted_recurrence(laplace3d)
@@ -16,23 +31,41 @@ def test_laplace_3D():
 
     var = _make_sympy_vec("x", 3)
     var_t = _make_sympy_vec("t", 3)
-    abs_dist = sp.sqrt((var[0]-var_t[0])**2 + (var[1]-var_t[1])**2 + (var[2]-var_t[2])**2)
+    abs_dist = sp.sqrt((var[0]-var_t[0])**2 +
+                       (var[1]-var_t[1])**2 + (var[2]-var_t[2])**2)
     g_x_y = 1/abs_dist
-    derivs = [sp.diff(g_x_y, var_t[0], i).subs(var_t[0], 0).subs(var_t[1], 0).subs(var_t[2], 0) for i in range(6)]
+    derivs = [sp.diff(g_x_y,
+                      var_t[0], i).subs(var_t[0], 0).subs(var_t[1], 0).subs(var_t[2], 0)
+                                               for i in range(6)]
 
-    check_2_s = r.subs(n, 2).subs(s(0), derivs[0]).subs(s(1), derivs[1]) - derivs[2]
-    check_3_s = r.subs(n, 3).subs(s(0), derivs[0]).subs(s(1), derivs[1]).subs(s(2), derivs[2]) - derivs[3]
-    check_4_s = r.subs(n, 4).subs(s(0), derivs[0]).subs(s(1), derivs[1]).subs(s(2), derivs[2]).subs(s(3), derivs[3]) - derivs[4]
-    check_5_s = r.subs(n, 5).subs(s(0), derivs[0]).subs(s(1), derivs[1]).subs(s(2), derivs[2]).subs(s(3), derivs[3]).subs(s(4), derivs[4]) - derivs[5]
+    # pylint: disable-next=not-callable
+    subs_dict = {s(0): derivs[0], s(1): derivs[1]}
+    check_2_s = r.subs(n, 2).subs(subs_dict) - derivs[2]
+    # pylint: disable-next=not-callable
+    subs_dict[s(2)] = derivs[2]
+    check_3_s = r.subs(n, 3).subs(subs_dict) - derivs[3]
+    # pylint: disable-next=not-callable
+    subs_dict[s(3)] = derivs[3]
+    check_4_s = r.subs(n, 4).subs(subs_dict) - derivs[4]
+    # pylint: disable-next=not-callable
+    subs_dict[s(4)] = derivs[4]
+    check_5_s = r.subs(n, 5).subs(subs_dict) - derivs[5]
 
-    assert abs(abs(check_2_s.subs(var[0], np.random.rand()).subs(var[1], np.random.rand()).subs(var[2], np.random.rand()))) <= 1e-15
-    assert abs(abs(check_3_s.subs(var[0], np.random.rand()).subs(var[1], np.random.rand()).subs(var[2], np.random.rand()))) <= 1e-14
-    assert abs(abs(check_4_s.subs(var[0], np.random.rand()).subs(var[1], np.random.rand()).subs(var[2], np.random.rand()))) <= 1e-12
-    #print(abs(abs(check_5_s.subs(var[0], np.random.rand()).subs(var[1], np.random.rand()).subs(var[2], np.random.rand()))))
-    assert abs(abs(check_5_s.subs(var[0], np.random.rand()).subs(var[1], np.random.rand()).subs(var[2], np.random.rand()))) <= 1e-12
+    x_coord = np.random.rand()  # noqa: NPY002
+    y_coord = np.random.rand()  # noqa: NPY002
+    z_coord = np.random.rand()  # noqa: NPY002
+    coord_dict = {var[0]: x_coord, var[1]: y_coord, var[2]: z_coord}
+
+    assert abs(check_2_s.subs(coord_dict)) <= 1e-15
+    assert abs(check_3_s.subs(coord_dict)) <= 1e-14
+    assert abs(check_4_s.subs(coord_dict)) <= 1e-12
+    assert abs(check_5_s.subs(coord_dict)) <= 1e-12
 
 
-def test_helmholtz_3D():
+def test_helmholtz3d():
+    r"""
+    Tests recurrence code for orders up to 6 helmholtz3d.
+    """
     w = make_identity_diff_op(3)
     helmholtz3d = laplacian(w) + w
     _, _, r = get_processed_and_shifted_recurrence(helmholtz3d)
@@ -42,27 +75,43 @@ def test_helmholtz_3D():
 
     var = _make_sympy_vec("x", 3)
     var_t = _make_sympy_vec("t", 3)
-    abs_dist = sp.sqrt((var[0]-var_t[0])**2 + (var[1]-var_t[1])**2 + (var[2]-var_t[2])**2)
+    abs_dist = sp.sqrt((var[0]-var_t[0])**2 +
+                       (var[1]-var_t[1])**2 + (var[2]-var_t[2])**2)
     g_x_y = sp.exp(1j * abs_dist) / abs_dist
-    derivs = [sp.diff(g_x_y, var_t[0], i).subs(var_t[0], 0).subs(var_t[1], 0).subs(var_t[2], 0) for i in range(6)]
+    derivs = [sp.diff(g_x_y,
+                      var_t[0], i).subs(var_t[0], 0).subs(var_t[1], 0).subs(var_t[2], 0)
+                                               for i in range(6)]
+
+    # pylint: disable-next=not-callable
+    subs_dict = {s(0): derivs[0], s(1): derivs[1]}
+    check_2_s = r.subs(n, 2).subs(subs_dict) - derivs[2]
+    # pylint: disable-next=not-callable
+    subs_dict[s(2)] = derivs[2]
+    check_3_s = r.subs(n, 3).subs(subs_dict) - derivs[3]
+    # pylint: disable-next=not-callable
+    subs_dict[s(3)] = derivs[3]
+    check_4_s = r.subs(n, 4).subs(subs_dict) - derivs[4]
+    # pylint: disable-next=not-callable
+    subs_dict[s(4)] = derivs[4]
+    check_5_s = r.subs(n, 5).subs(subs_dict) - derivs[5]
+
+    x_coord = np.random.rand()  # noqa: NPY002
+    y_coord = np.random.rand()  # noqa: NPY002
+    z_coord = np.random.rand()  # noqa: NPY002
+    coord_dict = {var[0]: x_coord, var[1]: y_coord, var[2]: z_coord}
+
+    assert abs(abs(check_2_s.subs(coord_dict))) <= 1e-15
+    assert abs(abs(check_3_s.subs(coord_dict))) <= 1e-14
+    assert abs(abs(check_4_s.subs(coord_dict))) <= 1e-12
+    assert abs(abs(check_5_s.subs(coord_dict))) <= 1e-12
 
 
-    check_2_s = r.subs(n, 2).subs(s(0), derivs[0]).subs(s(1), derivs[1]) - derivs[2]
-    check_3_s = r.subs(n, 3).subs(s(0), derivs[0]).subs(s(1), derivs[1]).subs(s(2), derivs[2]) - derivs[3]
-    check_4_s = r.subs(n, 4).subs(s(0), derivs[0]).subs(s(1), derivs[1]).subs(s(2), derivs[2]).subs(s(3), derivs[3]) - derivs[4]
-    check_5_s = r.subs(n, 5).subs(s(0), derivs[0]).subs(s(1), derivs[1]).subs(s(2), derivs[2]).subs(s(3), derivs[3]).subs(s(4), derivs[4]) - derivs[5]
-
-    assert abs(check_2_s.subs(var[0], np.random.rand()).subs(var[1], np.random.rand()).subs(var[2], np.random.rand())) <= 1e-15
-    assert abs(abs(check_3_s.subs(var[0], np.random.rand()).subs(var[1], np.random.rand()).subs(var[2], np.random.rand()))) <= 1e-14
-    assert abs(abs(check_4_s.subs(var[0], np.random.rand()).subs(var[1], np.random.rand()).subs(var[2], np.random.rand()))) <= 1e-12
-    assert abs(abs(check_5_s.subs(var[0], np.random.rand()).subs(var[1], np.random.rand()).subs(var[2], np.random.rand()))) <= 1e-12
-    
-
-
-def test_helmholtz_2D():
+def test_helmholtz2d():
+    r"""
+    Tests recurrence code for orders up to 6 helmholtz2d.
     w = make_identity_diff_op(2)
     helmholtz2d = laplacian(w) + w
-    _,_, r = get_processed_and_shifted_recurrence(helmholtz2d)
+    _, _, r = get_processed_and_shifted_recurrence(helmholtz2d)
 
     n = sp.symbols("n")
     s = sp.Function("s")
@@ -74,23 +123,21 @@ def test_helmholtz_2D():
     g_x_y = (1j/4) * hankel1(0, k * abs_dist)
     x_coord = np.random.rand()
     y_coord = np.random.rand()
-    derivs = [sp.diff(g_x_y, var_t[0], i).subs(var_t[0], 0).subs(var_t[1], 0) for i in range(6)]
-    derivs = [derivs[i].subs(var[0], x_coord).subs(var[1], y_coord).evalf() for i in range(6)]
+    derivs = [sp.diff(g_x_y, var_t[0], i).subs(var_t[0], 0).subs(var_t[1], 0)
+    for i in range(6)]
+    derivs = [derivs[i].subs(var[0], x_coord).subs(var[1], y_coord).evalf()
+    for i in range(6)]
+    """
+    print("HELLO!")
 
-    check_2_s = r.subs(n, 2).subs(s(0), derivs[0]).subs(s(1), derivs[1]) - derivs[2]
-    check_3_s = r.subs(n, 3).subs(s(0), derivs[0]).subs(s(1), derivs[1]).subs(s(2), derivs[2]) - derivs[3]
-    check_4_s = r.subs(n, 4).subs(s(0), derivs[0]).subs(s(1), derivs[1]).subs(s(2), derivs[2]).subs(s(3), derivs[3]) - derivs[4]
-    check_5_s = r.subs(n, 5).subs(s(0), derivs[0]).subs(s(1), derivs[1]).subs(s(2), derivs[2]).subs(s(3), derivs[3]).subs(s(4), derivs[4]) - derivs[5]
 
-    assert abs(check_2_s.subs(var[0],x_coord).subs(var[1],y_coord)) <= 1e-12
-    assert abs(check_3_s.subs(var[0],x_coord).subs(var[1],y_coord)) <= 1e-12
-    assert abs(check_4_s.subs(var[0],x_coord).subs(var[1],y_coord)) <= 1e-12
-    assert abs(check_5_s.subs(var[0],x_coord).subs(var[1],y_coord)) <= 1e-12
-
-def test_laplace_2D():
+def test_laplace2d():
+    r"""
+    Tests recurrence code for orders up to 6 laplace2d.
+    """
     w = make_identity_diff_op(2)
     laplace2d = laplacian(w)
-    _,_, r = get_processed_and_shifted_recurrence(laplace2d)
+    _, _, r = get_processed_and_shifted_recurrence(laplace2d)
 
     n = sp.symbols("n")
     s = sp.Function("s")
@@ -98,16 +145,31 @@ def test_laplace_2D():
     var = _make_sympy_vec("x", 2)
     var_t = _make_sympy_vec("t", 2)
     g_x_y = sp.log(sp.sqrt((var[0]-var_t[0])**2 + (var[1]-var_t[1])**2))
-    derivs = [sp.diff(g_x_y, var_t[0], i).subs(var_t[0], 0).subs(var_t[1], 0) for i in range(6)]
+    derivs = [sp.diff(g_x_y,
+                      var_t[0], i).subs(var_t[0], 0).subs(var_t[1], 0)
+                      for i in range(6)]
 
-    check_2_s = r.subs(n, 2).subs(s(1), derivs[1]) - derivs[2]
-    check_3_s = r.subs(n, 3).subs(s(1), derivs[1]).subs(s(2), derivs[2]) - derivs[3]
-    check_4_s = r.subs(n, 4).subs(s(1), derivs[1]).subs(s(2), derivs[2]).subs(s(3), derivs[3]) - derivs[4]
-    check_5_s = r.subs(n, 5).subs(s(1), derivs[1]).subs(s(2), derivs[2]).subs(s(3), derivs[3]).subs(s(4), derivs[4]) - derivs[5]
+    # pylint: disable-next=not-callable
+    subs_dict = {s(0): derivs[0], s(1): derivs[1]}
+    check_2_s = r.subs(n, 2).subs(subs_dict) - derivs[2]
+    # pylint: disable-next=not-callable
+    subs_dict[s(2)] = derivs[2]
+    check_3_s = r.subs(n, 3).subs(subs_dict) - derivs[3]
+    # pylint: disable-next=not-callable
+    subs_dict[s(3)] = derivs[3]
+    check_4_s = r.subs(n, 4).subs(subs_dict) - derivs[4]
+    # pylint: disable-next=not-callable
+    subs_dict[s(4)] = derivs[4]
+    check_5_s = r.subs(n, 5).subs(subs_dict) - derivs[5]
 
-    assert abs(check_2_s.subs(var[0], np.random.rand()).subs(var[1], np.random.rand())) <= 1e-15
-    assert abs(check_3_s.subs(var[0], np.random.rand()).subs(var[1], np.random.rand())) <= 1e-14
-    assert abs(check_4_s.subs(var[0], np.random.rand()).subs(var[1], np.random.rand())) <= 1e-12
-    assert abs(check_5_s.subs(var[0], np.random.rand()).subs(var[1], np.random.rand())) <= 1e-12
+    x_coord = np.random.rand()  # noqa: NPY002
+    y_coord = np.random.rand()  # noqa: NPY002
+    coord_dict = {var[0]: x_coord, var[1]: y_coord}
+
+    assert abs(abs(check_2_s.subs(coord_dict))) <= 1e-15
+    assert abs(abs(check_3_s.subs(coord_dict))) <= 1e-14
+    assert abs(abs(check_4_s.subs(coord_dict))) <= 1e-12
+    assert abs(abs(check_5_s.subs(coord_dict))) <= 1e-12
 
 
+test_laplace2d()
--- test/test_recurrenceqbx.py
+++ test/test_recurrenceqbx.py
@@ -1,69 +1,78 @@
+r"""
+With the functionality in this module, we aim to test recurrence
+code.
+"""
+from __future__ import annotations
+
 import numpy as np
 import sympy as sp
-from sympy import hankel1
 
+# from sympy import hankel1
+from sumpy.array_context import _acf
 from sumpy.expansion.diff_op import (
     laplacian,
     make_identity_diff_op,
 )
-from sumpy.recurrenceqbx import recurrence_qbx_lp, _make_sympy_vec
-from sumpy.array_context import PytestPyOpenCLArrayContextFactory, _acf  # noqa: F401
-from sumpy.expansion.local import LineTaylorLocalExpansion, VolumeTaylorLocalExpansion
-from sumpy.kernel import LaplaceKernel, HelmholtzKernel
+from sumpy.expansion.local import LineTaylorLocalExpansion
+from sumpy.kernel import HelmholtzKernel, LaplaceKernel
 from sumpy.qbx import LayerPotential
+from sumpy.recurrenceqbx import _make_sympy_vec, recurrence_qbx_lp
+
 
 actx_factory = _acf
-expn_class = LineTaylorLocalExpansion
+ExpnClass = LineTaylorLocalExpansion
 
 actx = actx_factory()
 lknl = LaplaceKernel(2)
 hlknl = HelmholtzKernel(2, "k")
 
-def _qbx_lp_helmholtz_general(sources,targets,centers,radius,strengths,order):
-        lpot = LayerPotential(actx.context,
-        expansion=expn_class(hlknl, order),
-        target_kernels=(hlknl,),
-        source_kernels=(hlknl,))
 
-        #print(lpot.get_kernel())
-        expansion_radii = actx.from_numpy(radius * np.ones(sources.shape[1]))
-        sources = actx.from_numpy(sources)
-        targets = actx.from_numpy(targets)
-        centers = actx.from_numpy(centers)
+def _qbx_lp_helmholtz_general(sources, targets, centers, radius, strengths, order):
+    lpot = LayerPotential(actx.context,
+    expansion=ExpnClass(hlknl, order),
+    target_kernels=(hlknl,),
+    source_kernels=(hlknl,))
 
-        strengths = (strengths,)
-        extra_kernel_kwargs={"k": 1}
-        _evt, (result_qbx,) = lpot(
-                actx.queue,
-                targets, sources, centers, strengths,
-                expansion_radii=expansion_radii,
-                kwargs=extra_kernel_kwargs)
-        result_qbx = actx.to_numpy(result_qbx)
+    # print(lpot.get_kernel())
+    expansion_radii = actx.from_numpy(radius * np.ones(sources.shape[1]))
+    sources = actx.from_numpy(sources)
+    targets = actx.from_numpy(targets)
+    centers = actx.from_numpy(centers)
 
-        return result_qbx
+    strengths = (strengths,)
+    extra_kernel_kwargs = {"k": 1}
+    _evt, (result_qbx,) = lpot(
+            actx.queue,
+            targets, sources, centers, strengths,
+            expansion_radii=expansion_radii,
+            kwargs=extra_kernel_kwargs)
+    result_qbx = actx.to_numpy(result_qbx)
+
+    return result_qbx
 
 
-def _qbx_lp_laplace_general(sources,targets,centers,radius,strengths,order):
-        lpot = LayerPotential(actx.context,
-        expansion=expn_class(lknl, order),
-        target_kernels=(lknl,),
-        source_kernels=(lknl,))
+def _qbx_lp_laplace_general(sources, targets, centers, radius, strengths, order):
+    lpot = LayerPotential(actx.context,
+    expansion=ExpnClass(lknl, order),
+    target_kernels=(lknl,),
+    source_kernels=(lknl,))
 
-        #print(lpot.get_kernel())
-        expansion_radii = actx.from_numpy(radius * np.ones(sources.shape[1]))
-        sources = actx.from_numpy(sources)
-        targets = actx.from_numpy(targets)
-        centers = actx.from_numpy(centers)
+    # print(lpot.get_kernel())
+    expansion_radii = actx.from_numpy(radius * np.ones(sources.shape[1]))
+    sources = actx.from_numpy(sources)
+    targets = actx.from_numpy(targets)
+    centers = actx.from_numpy(centers)
 
-        strengths = (strengths,)
+    strengths = (strengths,)
 
-        _evt, (result_qbx,) = lpot(
-                actx.queue,
-                targets, sources, centers, strengths,
-                expansion_radii=expansion_radii)
-        result_qbx = actx.to_numpy(result_qbx)
+    _evt, (result_qbx,) = lpot(
+            actx.queue,
+            targets, sources, centers, strengths,
+            expansion_radii=expansion_radii)
+    result_qbx = actx.to_numpy(result_qbx)
 
-        return result_qbx
+    return result_qbx
+
 
 def _create_ellipse(n_p):
     h = 9.688 / n_p
@@ -83,31 +92,40 @@ def _create_ellipse(n_p):
 
     return sources, centers, normals, density, h, radius
 
-def test_recurrence_laplace_2d_ellipse():
 
-    #------------- 1. Define PDE, Green's Function
+def test_recurrence_laplace_2d_ellipse():
+    r"""
+    Tests recurrence code for orders up to 6 laplace3d.
+    """
+
+    # ------------- 1. Define PDE, Green's Function
     w = make_identity_diff_op(2)
     laplace2d = laplacian(w)
 
     var = _make_sympy_vec("x", 2)
     var_t = _make_sympy_vec("t", 2)
-    g_x_y = (-1/(2*np.pi)) * sp.log(sp.sqrt((var[0]-var_t[0])**2 + (var[1]-var_t[1])**2))
+    g_x_y = (-1/(2*np.pi)) * sp.log(sp.sqrt((var[0]-var_t[0])**2 +
+                                            (var[1]-var_t[1])**2))
 
     p = 4
     err = []
     for n_p in range(200, 1001, 200):
         sources, centers, normals, density, h, radius = _create_ellipse(n_p)
         strengths = h * density
-        exp_res = recurrence_qbx_lp(sources, centers, normals, strengths, radius, laplace2d, g_x_y, 2, p)
-        qbx_res = _qbx_lp_laplace_general(sources, sources, centers, radius, strengths, p)
-        #qbx_res,_ = lpot_eval_circle(sources.shape[1], p)
+        exp_res = recurrence_qbx_lp(sources, centers, normals,
+                                    strengths, radius, laplace2d,
+                                    g_x_y, 2, p)
+        qbx_res = _qbx_lp_laplace_general(sources, sources, centers,
+                                          radius, strengths, p)
+        # qbx_res,_ = lpot_eval_circle(sources.shape[1], p)
         err.append(np.max(np.abs(exp_res - qbx_res)))
     assert np.max(err) <= 1e-13
 
 
 def test_recurrence_helmholtz_2d_ellipse():
-
-    #------------- 1. Define PDE, Green's Function
+    r"""
+    Tests recurrence code for orders up to 6 laplace3d.
+    # ------------- 1. Define PDE, Green's Function
     w = make_identity_diff_op(2)
     helmholtz2d = laplacian(w) + w
 
@@ -118,14 +136,16 @@ def test_recurrence_helmholtz_2d_ellipse():
     g_x_y = (1j/4) * hankel1(0, k * abs_dist)
 
     p = 4
-    err = []
+    # err = []
     for n_p in range(200, 1001, 200):
         sources, centers, normals, density, h, radius = _create_ellipse(n_p)
         strengths = h * density
-        exp_res = recurrence_qbx_lp(sources, centers, normals, strengths, radius, helmholtz2d, g_x_y, 2, p)
-        #qbx_res = _qbx_lp_helmholtz_general(sources, sources, centers, radius, strengths, p)
+        exp_res = recurrence_qbx_lp(sources, centers, normals, strengths,
+        radius, helmholtz2d, g_x_y, 2, p)
+        #qbx_res = _qbx_lp_helmholtz_general(sources, sources, centers,
+        # radius, strengths, p)
         #qbx_res,_ = lpot_eval_circle(sources.shape[1], p)
         #err.append(np.max(np.abs(exp_res - qbx_res)))
     #assert np.max(err) <= 1e-13
-
-test_recurrence_helmholtz_2d_ellipse()
+    """
+    print("Hello")
