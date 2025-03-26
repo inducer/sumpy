Culprit for error found, bug in n =1 off-axis expression for derivative

sumpy/recurrence_qbx.py
@@ -194,13 +194,14 @@ def recurrence_qbx_lp(sources, centers, normals, strengths, radius, pde, g_x_y,
 
         return sp.lambdify(arg_list, lamb_expr_symb)
 
+
     interactions_off_axis = 0
     for i in range(p+1):
         lamb_expr_t_recur = gen_lamb_expr_t_recur(i, start_order)
         a1 = [*storage_taylor[(-t_recur_order):], *coord]
 
-        storage.pop(0)
-        storage.append(lamb_expr_t_recur(*a1))
+        storage_taylor.pop(0)
+        storage_taylor.append(lamb_expr_t_recur(*a1) + np.zeros((n_p, n_p)))
 
         lamb_expr_t_exp = gen_lamb_expr_t_exp(i, t_exp_order)
         a2 = [*storage_taylor[-(t_exp_order+1):], *coord]
@@ -209,6 +210,7 @@ def recurrence_qbx_lp(sources, centers, normals, strengths, radius, pde, g_x_y,
 
     ################
     # Compute True Interactions
+    storage_taylor_true = [np.zeros((n_p, n_p))] * storage_taylor_order
     def generate_true(i):
         arg_list = []
         for j in range(ndim):
@@ -237,8 +239,6 @@ def recurrence_qbx_lp(sources, centers, normals, strengths, radius, pde, g_x_y,
     mask_on_axis = m*np.abs(coord[0]) >= np.abs(coord[1])
     mask_off_axis = m*np.abs(coord[0]) < np.abs(coord[1])
 
-
-  
     print("-------------------------")
 
     percent_on = np.sum(mask_on_axis)/(mask_on_axis.shape[0]*mask_on_axis.shape[1])

test/test_recurrence.py
@@ -287,8 +287,8 @@ def test_laplace_2d_off_axis(deriv_order, exp_order):
                         for i in range(max_deriv)]
     s = sp.Function("s")
 
-    x_coord = 0.0002934165818751001#1e-2 * np.random.rand()  # noqa: NPY002
-    y_coord = 0.06272081418069221#np.random.rand()  # noqa: NPY002
+    x_coord = 0.0006490822305146929#1e-2 * np.random.rand()  # noqa: NPY002
+    y_coord = -0.06766742499535426#np.random.rand()  # noqa: NPY002
     coord_dict = {var[0]: x_coord, var[1]: y_coord}
 
     w = make_identity_diff_op(2)
@@ -338,7 +338,7 @@ def test_laplace_2d_off_axis(deriv_order, exp_order):
     assert relerr <= prederror
     return relerr
 
-test_laplace_2d_off_axis(7, 8)
+test_laplace_2d_off_axis(1, 8)
 
 
 """ 